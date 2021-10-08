67627e37d159b08d377af9ef0f550023b0a38af7
Sam-Dunne <cras.sdunne@gmail.com>
2021-10-07 21:07:28 -0500

    still trying to get it to build

--- components/AboutContent.js
+++ components/AboutContent.js
@@ -2,26 +2,26 @@ import { Container, Section, HeroH1, ImageWrapper, SCImage } from "../globalstyl
 
 
 
-const AboutContent = ({ picFirst, mrgnLeftLG }) => {
+const AboutContent = ({  }) => {
     return (
         <>
             <Container>
                 <Section>
-                    <HeroH1>Hi, I'm Sam! This is my dog, Goof. <br /> I am a Junior Dev, who enjoys learning and problem-solving. <br /> I'm seeking a workplace to grow and be challenged.</HeroH1>
+                    <HeroH1>Hi, I'&apos;m Sam! This is my dog, Goof. <br /> I am a Junior Dev, who enjoys learning and problem-solving. <br /> I'&apos;m seeking a workplace to grow and be challenged.</HeroH1>
                     <ImageWrapper>
-                        <SCImage src="/invertedGoof.jpg" alt="Picture of upside down dog" width={600} height={400} objectFit='cover' priority={true} />
+                        <SCImage src="/invertedGoof.jpg" alt="Picture of upside down dog" width={600} height={400} objectFit="cover" priority={true} />
                     </ImageWrapper>
                 </Section>
                 <Section picFirst>
-                    <HeroH1 mrgnLeftLG>I started learning to code during Pandemic, and then attended Covalence's FullStack Bootcamp. It was a great and challenging experience!</HeroH1>
+                    <HeroH1 mrgnLeftLG>I started learning to code during Pandemic, and then attended Covalence'&apos;s FullStack Bootcamp. It was a great and challenging experience!</HeroH1>
                     <ImageWrapper>
-                        <SCImage src="/goof-in-grass.jpg" alt="dog chewing stick in the grass" width={600} height={400} objectFit='cover' priority={true} />
+                        <SCImage src="/goof-in-grass.jpg" alt="dog chewing stick in the grass" width={600} height={400} objectFit="cover" priority={true} />
                     </ImageWrapper>
                 </Section>
                 <Section>
                     <HeroH1>Prior to Pandemic, I worked in Audio for Live Events and Concerts. So I have experience working in dynamic fast paced environments in teams, leading those teams, and getting along with widely diverse people. </HeroH1>
                     <ImageWrapper>
-                        <SCImage src="/dliveConsole.png" alt="audio mixing console" width={600} height={400} objectFit='cover' priority={true} />
+                        <SCImage src="/dliveConsole.png" alt="audio mixing console" width={600} height={400} objectFit="cover" priority={true} />
                     </ImageWrapper>
                 </Section>
             </Container>
@@ -29,4 +29,4 @@ const AboutContent = ({ picFirst, mrgnLeftLG }) => {
     )
 }
 
-export default AboutContent
+export default AboutContent;
