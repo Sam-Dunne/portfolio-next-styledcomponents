74158d9d27be7c4307d60bdbcd8bad8d783b312e
Sam-Dunne <cras.sdunne@gmail.com>
2022-04-26 18:06:01 -0500

    commenting out breaky code

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,35 +1,35 @@
-import { SMTPClient } from 'emailjs';
+// import { SMTPClient } from 'emailjs';
  
  
-export default function handler(req, res) {
+// export default function handler(req, res) {
  
- const {email}=req.body;
- // console.log(process.env)
+//  const {email}=req.body;
+//  // console.log(process.env)
  
- const client = new SMTPClient({
-   user: process.env.mail,
-   password: process.env.password,
-   host: 'smtp.gmail.com',
-   ssl:true
- });
+//  const client = new SMTPClient({
+//    user: process.env.mail,
+//    password: process.env.password,
+//    host: 'smtp.gmail.com',
+//    ssl:true
+//  });
  
- try{
+//  try{
  
   
-   client.send(
-     {
-       text: `Just for testing purpose`,
-       from: process.env.mail,
-       to: email,
-       subject: 'testing emailjs',
+//    client.send(
+//      {
+//        text: `Just for testing purpose`,
+//        from: process.env.mail,
+//        to: email,
+//        subject: 'testing emailjs',
       
-     }
-     )
-   }
- catch(e){
-   res.status(400).end(JSON.stringify({ message:"Error" }))
-   return;
- }
+//      }
+//      )
+//    }
+//  catch(e){
+//    res.status(400).end(JSON.stringify({ message:"Error" }))
+//    return;
+//  }
  
- res.status(200).end(JSON.stringify({ message:'Send Mail' }))
-}
+//  res.status(200).end(JSON.stringify({ message:'Send Mail' }))
+// }
