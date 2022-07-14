4cc557058dd0ce0f8830109cc6ec0538d62c9947
Sam-Dunne <cras.sdunne@gmail.com>
2022-07-14 07:06:19 -0500

    updating resume file

--- components/DownloadPDFLink/DownloadPDFLink.js
+++ components/DownloadPDFLink/DownloadPDFLink.js
@@ -4,7 +4,7 @@ import { PDFLinkWrapper, DownloadPDF } from './DownloadPDFLink.elements'
 const DownloadPDFLink = ({ }) => {
     return (
         <PDFLinkWrapper>
-            <DownloadPDF href="/2022-07-04 SD Resume.pdf" download="Sam Dunne Resume">Download Resume</DownloadPDF>
+            <DownloadPDF href="/2022-07-12 Sam Dunne WebDev Resume.pdf" download="Sam Dunne Resume">Download Resume</DownloadPDF>
         </PDFLinkWrapper>
     )
 }
