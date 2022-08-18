a003c813410bd3ee6fd00111270db0eef8275fe6
Sam-Dunne <cras.sdunne@gmail.com>
2022-08-18 09:19:29 -0500

    adding single source of truth for StackIcons font-size across breakpoints

--- components/StackIcons/StackIcons.elements.js
+++ components/StackIcons/StackIcons.elements.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 import {device} from '../../device';
 import {SiHtml5, SiCss3, SiReact, SiRedux, SiTailwindcss, SiBootstrap, SiGit, SiJavascript, SiStyledComponents, SiNextDotJs} from 'react-icons/si'
 // import {TbBrandNextjs} from 'react-icons/tb'
+const fontSize = {
+    mobile: '2.4em',
+    tablet: '4em',
+    laptopL: '4.2em'
+};
 
 export const IconGrid = styled.div`
     display: grid;
@@ -20,141 +25,142 @@ export const IconGrid = styled.div`
 
 export const IconHTML = styled(SiHtml5)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconCSS = styled(SiCss3)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconREACT = styled(SiReact)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconREDUX = styled(SiRedux)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconTAILWIND = styled(SiTailwindcss)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconBOOTSTRAP = styled(SiBootstrap)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconGIT = styled(SiGit)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconJS = styled(SiJavascript)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
 export const IconSTYLED = styled(SiStyledComponents)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
+
 export const IconNEXT = styled(SiNextDotJs)`
     color: #6600CC;
-    font-size: 2.4em;
+    font-size: ${fontSize.mobile};
     margin: 0 auto;
 
     @media ${device.tablet} {
-    font-size: 4em;    
+    font-size: ${fontSize.tablet};    
     }
 
     @media ${device.laptopL} {
-    font-size: 4.5em;
+    font-size: ${fontSize.laptopL};
     }
 `;
 
