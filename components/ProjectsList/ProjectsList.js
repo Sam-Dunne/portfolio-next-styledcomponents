29ba19ac846d656af5a74cea7e3dbaf5dce5acd5
Sam-Dunne <cras.sdunne@gmail.com>
2022-08-18 09:40:19 -0500

    removing unused code block from ProjectsList

--- components/ProjectsList/ProjectsList.js
+++ components/ProjectsList/ProjectsList.js
@@ -2,28 +2,6 @@ import { HeaderTitle } from '../../globalstyle';
 import { ProjectsSection } from './ProjectsList.elements';
 import ProjectsCardCO from '../../components/ProjectsCardCO/ProjectsCardCO';
 
-
-
-// const ProjectsSection = styled.section`
-//     position: relative;
-//     padding: 0px 0;
-//     display: flex;
-//     justify-content: space-around;
-//     align-items: center;  
-//     flex-wrap: wrap;
-    
-//     @media ${device.tablet} {
-//         padding: 0 4em;
-//     }
-
-//     @media ${device.laptopL} {
-//         padding: 0 15vw;
-//     }
-//     `;
-
-
-
-
 const ProjectsList = ({ data }) => {
     return (
         <div>
